27079ddf602fe3992bba2b0c74056964a40b2874
Ray <raistlin.chen@163.com>
2021-12-21 14:23:46 +0800

    Initial Commit

---
+++ OpencvNotes/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpencvNotes
+//
+//  Created by Ray on 2021/12/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
